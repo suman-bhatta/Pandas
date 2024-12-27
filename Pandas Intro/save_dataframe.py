e79d7b43700eef324c717ca716791ecd7c9d6148
Suman Bhatta <sumanbhatta735@gmail.com>
2024-12-27 16:33:34 +0545

    Add additional DataFrame example and print output

--- Pandas Intro/save_dataframe.py
+++ Pandas Intro/save_dataframe.py
@@ -10,3 +10,16 @@ mydataset = {
 myvar = pandas.DataFrame(mydataset)
 
 print(myvar)
+
+
+
+import pandas as pd
+
+mydataset = {   
+    'cars': ["BMW", "Volvo", "Ford"],
+    'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
