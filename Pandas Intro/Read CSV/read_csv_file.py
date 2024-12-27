a722c490ae0102c1b19919302ba290a01f18bd42
Suman Bhatta <sumanbhatta735@gmail.com>
2024-12-27 17:05:12 +0545

    Rename load_csv_file.py to read_csv_file.py and update print statement for better readability

--- Pandas Intro/Read CSV/read_csv_file.py
+++ Pandas Intro/Read CSV/read_csv_file.py
@@ -1,5 +1,9 @@
 import pandas as pd
-
 df = pd.read_csv('D:/Learning/Pandas/Pandas Intro/Read CSV/data.csv')
-
 print(df.to_string())
+
+
+
+import pandas as pd
+df = pd.read_csv('D:/Learning/Pandas/Pandas Intro/Read CSV/data.csv')
+print(df) 
